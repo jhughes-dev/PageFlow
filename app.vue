20dddc9eff6cb83b20aeafa80278b1acaadacc0a
Jeremy Hughes <hcsgames@gmail.com>
2022-10-11 21:35:39 -0400

    Initial Submit

---
+++ app.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="center">
+        <h1 class="center">Page Flow</h1>
+        <span class="center">
+            The goal is to break up the contents of the page into new pages to
+            make the disply look like printable pages.
+        </span>
+        <PageFlow>
+            <p v-for="i in 20" :key="i">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil
+                deserunt repudiandae veritatis, suscipit officia quia
+                exercitationem eaque ducimus. Nisi, asperiores! Lorem, ipsum
+                dolor sit amet consectetur adipisicing elit. Repudiandae minima
+                adipisci doloribus optio ipsa totam voluptatibus maxime,
+                molestiae quae tenetur illum esse vero. Qui ratione voluptatem
+                ipsam delectus, quasi dolorem. Voluptate quam, mollitia, tempore
+                animi maiores incidunt fugit nam, corrupti distinctio quo
+                maxime. Eveniet, saepe ipsa autem at totam vero ab esse ipsam
+                distinctio odio ratione amet aliquid quam nisi quos repudiandae
+                culpa exercitationem praesentium unde earum debitis minima cum.
+                Labore minus rerum aspernatur ad? Sequi, ad maxime quaerat
+                libero dicta ipsam vero accusantium at ut nobis ab culpa
+                assumenda consequuntur voluptates quas praesentium repellendus
+                exercitationem totam, impedit veniam eligendi.
+            </p>
+        </PageFlow>
+    </div>
+</template>
+
+<style>
+body {
+    background-color: black;
+    color: white;
+    min-width: 100vw;
+    height: 100vh;
+}
+.center {
+    margin: 0 auto;
+}
+</style>
---
+++ components/PageFlow.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+const aspectRatio = "8.5/11";
+const height = "80vh";
+const margins = "1in";
+</script>
+
+<template>
+    <div class="page">
+        <slot />
+    </div>
+</template>
+
+<style scoped lang="scss">
+.page {
+    aspect-ratio: v-bind(aspectRatio);
+    height: v-bind(height);
+    padding: v-bind(margins);
+    margin: 0.5in auto;
+    overflow-y: hidden;
+    box-shadow: 0px 0px 20px 10px grey;
+    color: black;
+    background-color: white;
+}
+</style>
