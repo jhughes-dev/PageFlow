97bb0629cc4de6ee0c08100be0dc479f4c44a6ea
Jeremy Hughes <hcsgames@gmail.com>
2022-10-11 21:50:39 -0400

    Make props for PageFlow

--- app.vue
+++ app.vue
@@ -1,40 +1,66 @@
 <template>
-    <div class="center">
-        <h1 class="center">Page Flow</h1>
-        <span class="center">
-            The goal is to break up the contents of the page into new pages to
-            make the disply look like printable pages.
-        </span>
-        <PageFlow>
-            <p v-for="i in 20" :key="i">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil
-                deserunt repudiandae veritatis, suscipit officia quia
-                exercitationem eaque ducimus. Nisi, asperiores! Lorem, ipsum
-                dolor sit amet consectetur adipisicing elit. Repudiandae minima
-                adipisci doloribus optio ipsa totam voluptatibus maxime,
-                molestiae quae tenetur illum esse vero. Qui ratione voluptatem
-                ipsam delectus, quasi dolorem. Voluptate quam, mollitia, tempore
-                animi maiores incidunt fugit nam, corrupti distinctio quo
-                maxime. Eveniet, saepe ipsa autem at totam vero ab esse ipsam
-                distinctio odio ratione amet aliquid quam nisi quos repudiandae
-                culpa exercitationem praesentium unde earum debitis minima cum.
-                Labore minus rerum aspernatur ad? Sequi, ad maxime quaerat
-                libero dicta ipsam vero accusantium at ut nobis ab culpa
-                assumenda consequuntur voluptates quas praesentium repellendus
-                exercitationem totam, impedit veniam eligendi.
-            </p>
-        </PageFlow>
+    <div class="columns">
+        <div class="info">
+            <h1>Page Flow</h1>
+            <span>
+                The goal is to break up the contents of the page into new pages
+                to make the disply look like printable pages.
+                I.e. the content flows across the pages.
+            </span>
+            <br/>
+            <br/>
+            <span>
+            This is just an example.
+            </span>
+        </div>
+        <div class="page-content">
+            <PageFlow aspect="8.5/11" height="6in" margin="1in">
+                <p v-for="i in 20" :key="i">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                    Nihil deserunt repudiandae veritatis, suscipit officia quia
+                    exercitationem eaque ducimus. Nisi, asperiores! Lorem, ipsum
+                    dolor sit amet consectetur adipisicing elit. Repudiandae
+                    minima adipisci doloribus optio ipsa totam voluptatibus
+                    maxime, molestiae quae tenetur illum esse vero. Qui ratione
+                    voluptatem ipsam delectus, quasi dolorem. Voluptate quam,
+                    mollitia, tempore animi maiores incidunt fugit nam, corrupti
+                    distinctio quo maxime. Eveniet, saepe ipsa autem at totam
+                    vero ab esse ipsam distinctio odio ratione amet aliquid quam
+                    nisi quos repudiandae culpa exercitationem praesentium unde
+                    earum debitis minima cum. Labore minus rerum aspernatur ad?
+                    Sequi, ad maxime quaerat libero dicta ipsam vero accusantium
+                    at ut nobis ab culpa assumenda consequuntur voluptates quas
+                    praesentium repellendus exercitationem totam, impedit veniam
+                    eligendi.
+                </p>
+            </PageFlow>
+        </div>
     </div>
 </template>
 
-<style>
+<style lang="scss">
 body {
     background-color: black;
     color: white;
     min-width: 100vw;
-    height: 100vh;
 }
-.center {
+.columns {
     margin: 0 auto;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    & > * {
+        align-self: center;
+
+    }
+}
+.info {
+  margin: 1rem;
+  width: 200px;
+
+}
+.page-content{
+  margin: 1rem;
+  overflow-y: hidden;
 }
 </style>
--- components/PageFlow.vue
+++ components/PageFlow.vue
@@ -1,7 +1,14 @@
 <script setup lang="ts">
-const aspectRatio = "8.5/11";
-const height = "80vh";
-const margins = "1in";
+type props = {
+    aspect: string,
+    height: string,
+    margin: string,
+};
+withDefaults(defineProps<props>(), {
+    aspect: "8.5/11",
+    height: "11in",
+    margin: "1in",
+});
 </script>
 
 <template>
@@ -12,10 +19,10 @@ const margins = "1in";
 
 <style scoped lang="scss">
 .page {
-    aspect-ratio: v-bind(aspectRatio);
+    aspect-ratio: v-bind(aspect);
     height: v-bind(height);
-    padding: v-bind(margins);
-    margin: 0.5in auto;
+    padding: v-bind(margin);
+    margin: 0.5in;
     overflow-y: hidden;
     box-shadow: 0px 0px 20px 10px grey;
     color: black;
